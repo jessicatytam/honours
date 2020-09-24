174c1fb8abee6499589b35e59769dc8c15313650
Will Cornwell <wcornwell@gmail.com>
2020-09-24 10:44:18 +1000

    Getting median latitudes from gbif, filtered to rotl species list

--- R/gbif_process.R
+++ R/gbif_process.R
@@ -2,6 +2,7 @@ library(data.table)
 library(dplyr)
 library(readr)
 library(stringr)
+library(ggplot2)
 
 z<-fread("data/0062362-200613084148143.csv")
 z2<-select(z,order,family,genus,species,scientificName,decimalLatitude,decimalLongitude,year,issue)
@@ -20,9 +21,24 @@ z4<-select(z3,scientificName,decimalLatitude,decimalLongitude,year)
 
 write_csv(z4,"data/gbif_processed.csv")
 
-gbif <- read.csv(file = "gbif_processed.csv", header = T)
+gbif <- read_csv(file = "data/gbif_processed.csv")
 
-unique(gbif$scientificName) #41850 records
+length(unique(gbif$species)) #41850 records
 
-gbif %>%
-  mutate(species = word(scientificName, 1, 2, sep = " "))
+#fron phylogenetic tree.R
+sbs<-filter(gbif,species %in% finalspecieslist$specieslist)
+
+length(unique(sbs$species))
+
+sbs %>%
+  group_by(species) %>%
+  summarize(median_lat=median(decimalLatitude,na.rm=T)) %>%
+  filter(!is.na(median_lat)) %>%
+  write_csv("data/gbif_lat_medians.csv")->meds
+
+tax<-select(z3,order,species) %>%
+  distinct()
+
+meds<-left_join(meds,tax)
+
+ggplot(meds,aes(x=median_lat,fill=order))+geom_histogram()+theme_bw()
