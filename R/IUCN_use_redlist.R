d4181d6ff6056387a2bea3e7de9429328a8e7932
jessicatytam <z5093095@student.unsw.edu.au>
2020-12-09 15:58:19 +1100

    still trying and doing some test

--- R/IUCN_use_redlist.R
+++ R/IUCN_use_redlist.R
@@ -27,3 +27,34 @@ newlist <- meltedList %>%
  
 #red list category columns
 
+redlist <- newlist %>%
+  mutate(redlistCategory2 = NA) #add extra red list column
+redlist <- redlist[c(1,2,4,3)] #rearrange columns
+
+for (i in 1:count(redlist)-1){
+  if(redlist[i,][1]==redlist[i+1,][1] & redlist[i,][4]==redlist[i+1,][4]){ #if species name and use1 are the same for 2 rows
+    replace(redlist, redlist[i,][3], redlist[i+1,][3]) #replace NA with the 2nd redlist value
+    redlist <- redlist[-c(i+1),] #remove the 2nd row
+  }
+}
+
+
+
+#use columns
+
+
+
+
+
+test <- matrix(c(1,1,2,2,3,3,NA,NA,NA,4,4,4),
+               nrow = 3)
+test
+test[-1,]
+
+for (i in 1:count(test)-1){
+  n <- i+1
+  if(test[i,][1]==test[n,][1] && test[i,][4]==test[n,][4]){
+    replace(test, test[i,][3], test[n,][3])
+  }
+}
+
