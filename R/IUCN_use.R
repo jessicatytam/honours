edd86963762eb85363e73512b02a58950395c3d4
jessicatytam <z5093095@student.unsw.edu.au>
2020-12-16 14:04:55 +1100

    replacing numbers with the actual uses

--- R/IUCN_use.R
+++ R/IUCN_use.R
@@ -2,6 +2,7 @@ library(reshape2)
 library(dplyr)
 library(tidyr)
 library(readr)
+library(stringr)
 
 #import the data
 
@@ -29,66 +30,17 @@ newlist <- meltedList[c(1,3)] %>%
 
 #trying pivot_wider
 
-sum(newlist$scientificName == "Acinonyx jubatus")
-
-for (i in 1:length(newlist)) {
-  sum(newlist$scientificName[i])
-}
-
 newlistaddcol <- newlist %>%
-  mutate(uselabel = )
+  mutate(sppcount = sequence(rle(newlist$scientificName)$lengths))
 
-test <- newlist %>%
-  pivot_wider(names_from = use,
+newlistnum <- newlistaddcol %>% #1472
+  pivot_wider(names_from = sppcount,
               values_from = use)
 
+#replace numbers with uses
 
+Uses <- list.files(path = "IUCN_use", full.names = FALSE)
+Uses <- data.frame(gsub("\\..*","", Uses)) #remove ".csv"
 
-
-
-#red list category columns
-
-redlist <- newlist %>%
-  mutate(redlistCategory2 = NA) #add extra red list column
-redlist <- redlist[c(1,2,4,3)] #rearrange columns
-
-for(i in 1:2466){
-  for (j in 2:2465) {
-    if(redlist[i,][1] %in% redlist[j,][1] & redlist[i,][4] %in% redlist[j,][4]){ #if species name & use are the same for 2 rows
-      replace(redlist, redlist$redlistCategory2[i], redlist$redlistCategory[j]) #replace NA of the 1st row with the 2nd row's red list value
-      redlist <- redlist[-j,] #remove the 2nd row
-    }
-  }
-}
-
-
-class(redlist$redlistCategory)
-
-
-
-test <- matrix(c(1,1,2,2,3,3,NA,NA,NA,4,4,4),
-               nrow = 3)
-test
-nrow(test)
-test[-1,]
-
-#for loop
-for(i in 1:(nrow(test)-1)){
-  for(j in 2:(nrow(test)-2)){
-    if(identical(test[i,][1], test[j,][1]) & identical(test[i,][4], test[j,][4])){
-    test[test[i,][3] == "NA"] <- test[j][2] #replacing not working
-    test <- test[-j,] #removing works
-    }
-  }
-}
-
-#while loop
-i <- 1
-while(i < nrow(test)){
-  if(test[i,][1] %in% test[i+1,][1] & test[i,][4] %in% test[i+1,][4]){
-    replace(test, test[i,][3], test[i+1,][2])
-    test <- test[-(i+1),]
-  }
-  i <- i+1
-}
+str_replace_all(newlistnum, "\\d+", Uses)
 
