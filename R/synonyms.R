b3a8eb4123172e5fe2a22994338ae3963a3accb0
jessicatytam <z5093095@student.unsw.edu.au>
2021-01-14 10:39:16 +1100

    added rotl id

--- R/synonyms.R
+++ R/synonyms.R
@@ -21,27 +21,56 @@ synonyms <- synonyms %>% rename(synonyms = ...1)
 
 #get species ID
 
-tnrs_match_names(synonyms$...1)
+id <- data.frame()
+for(i in 1:nrow(synonyms)) {
+  getID <- tnrs_match_names(synonyms$synonyms[i])
+  getID <- data.frame(getID$ott_id)
+  id <- bind_rows(id, getID)
+  print(paste("Done:", synonyms$synonyms[i]))
+}
 
+synonyms <- cbind(synonyms, id)
+synonyms <- synonyms %>% rename(id = getID.ott_id)
+
+phyloid <- data.frame()
+for(i in 1:nrow(phylo)) {
+  getID <- tnrs_match_names(phylo$species[i])
+  getID <- data.frame(getID$ott_id)
+  phyloid <- bind_rows(phyloid, getID)
+  print(paste("Done:", phylo$species[i]))
+}
+
+phylo <- cbind(phylo, phyloid)
+phylo <- phylo %>% rename(id = getID.ott_id)
 
 #saving files
 
 write.csv(synonyms, file = "intermediate_data/synonyms.csv")
-
+write.csv(phylo, file = "intermediate_data/species_list_from_phylo.csv")
 
 #testing
 
 kangaroo <- data.frame(synonyms(tnrs_match_names("Macropus rufus")))
 data.frame(synonyms(tnrs_match_names("Abrocoma vaccarum")))
 
-test <- data.frame(phylo[1:10,])
+test <- data.frame(synonyms[1:10,])
 
-synonyms <- data.frame()
+tnrs_match_names(test$synonyms.1.10...[7])$ott_id
+
 for (i in 1:nrow(test)) {
-  results <- synonyms(tnrs_match_names(test$phylo.1.10...))[i]
-  resultsdf <- data.frame(results)
-  #colnames(resultsdf) = colnames(synonyms)
-  synonyms <- bind_rows(synonyms, resultsdf)
+  getID <- tnrs_match_names(test$synonyms.1.10...[i])
+  ID <- data.frame(getID)[4]
+  testID <- test %>%
+    mutate(ott_id = ID[i])
 }
 
-is.atomic(results)
+id <- data.frame()
+for(i in 1:nrow(test)) {
+  getID <- tnrs_match_names(test$synonyms.1.10...[i])
+  getID <- data.frame(getID$ott_id)
+  id <- bind_rows(id, getID)
+  print(paste("Done:", test$synonyms.1.10...[i]))
+}
+
+testID <- test %>%
+  mutate(ott_id = tnrs_match_names(synonyms.1.10...$ott_id))
