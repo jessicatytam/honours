d1efab901ccb54664f51a50a58d126adbf61e778
jessicatytam <z5093095@student.unsw.edu.au>
2021-01-20 11:35:54 +1100

    some testing

--- R/mammal_list_query_script.R
+++ R/mammal_list_query_script.R
@@ -5,22 +5,21 @@ syn <- read_csv("intermediate_data/synonyms.csv")
 sp <- read_csv("intermediate_data/species_list_from_phylo.csv")
 
 sp$species[16]
-
 syn$synonyms[match(sp$id[16], syn$id)]
 
 #checking that synomym search works
 FetchSpTAK(genus = "Abrothrix", species = "sanborni", synonyms = "Akodon sanborni", APIkey = "442b9048417ef20cf680a0ae26ee4d86")
 
+#add quotation marks around synonyms
+syn$synonyms <- shQuote(syn$synonyms, "cmd")
 
-#for(i in 1:length(sp$species)){
-
-
+#list to get citation records
 scopus_out <- list() #initializing empty lists
-for (i in 1:100) {  
+for (i in 1:20) {  
   if (!sp$id[i] %in% syn$id){
     scopus_out[[i]] <- FetchSpTAK(genus = str_split(sp$species[i], pattern = " ")[[1]][1],
-               species = str_split(sp$species[i], pattern = " ")[[1]][2],
-               APIkey = "442b9048417ef20cf680a0ae26ee4d86")
+                                  species = str_split(sp$species[i], pattern = " ")[[1]][2],
+                                  APIkey = "442b9048417ef20cf680a0ae26ee4d86")
   }
   else {
    syns <- syn$synonyms[match(sp$id[i], syn$id)]
@@ -33,13 +32,43 @@ for (i in 1:100) {
 
 saveRDS(scopus_out, "intermediate_data/temp_scopus_results.RDS")
 
-#lapply(scopus_out,Allindices,genus="genus",species="species")
 
 #currently getting at error at index 14 and also later on
 ai <- list()
-for (i in c(1:13,15:27)){
-  ai[[i]]<-Allindices(scopus_out[[i]],genus=str_split(sp$species[i],pattern = " ")[[1]][1],species=str_split(sp$species[i],pattern = " ")[[1]][2])
+for (i in c(1:20)){
+  ai[[i]] <- Allindices(scopus_out[[i]],
+                        genus = str_split(sp$species[i], pattern = " ")[[1]][1],
+                        species=str_split(sp$species[i], pattern = " ")[[1]][2])
 }
 
-ai_df<-bind_rows(ai)
+ai_df <- bind_rows(ai)
+
+
+
+
+
+
+#testing
+
+test <- syn[1:10,]
+
+for (i in 1:10) {
+  test$synonyms <- shQuote(test$synonyms, "cmd")
+}
+
+test_scopus_out <- list() #initializing empty lists
+for (i in 1:nrow(test)) {  
+  if (!sp$id[i] %in% test$id){
+    scopus_out[[i]] <- FetchSpTAK(genus = str_split(sp$species[i], pattern = " ")[[1]][1],
+                                  species = str_split(sp$species[i], pattern = " ")[[1]][2],
+                                  APIkey = "442b9048417ef20cf680a0ae26ee4d86")
+  }
+  else {
+    syns <- syn$synonyms[match(sp$id[i], syn$id)]
+    scopus_out[[i]] <- FetchSpTAK(genus = str_split(sp$species[i], pattern = " ")[[1]][1],
+                                  species = str_split(sp$species[i], pattern = " ")[[1]][2],
+                                  synonyms = syns,
+                                  APIkey = "442b9048417ef20cf680a0ae26ee4d86")
+  }
+}
 
