303be751440304fa491859d46542e97448173163
jessicatytam <z5093095@student.unsw.edu.au>
2021-01-21 14:00:13 +1100

    had to chop mammals list in half to avoid time out error

--- R/mammal_list_query_script.R
+++ R/mammal_list_query_script.R
@@ -3,7 +3,8 @@ devtools::install_github("jessicatytam/specieshindex", force = TRUE, build_vigne
 library(specieshindex)
 
 syn <- read_csv("intermediate_data/synonyms.csv")
-sp <- read_csv("intermediate_data/species_list_from_phylo.csv")
+sp1 <- read_csv("intermediate_data/species_list_from_phylo.csv")[1:3500,]
+sp2 <- read_csv("intermediate_data/species_list_from_phylo.csv")[3501:6949,]
 
 sp$species[16]
 syn$synonyms[match(sp$id[16], syn$id)]
@@ -15,33 +16,59 @@ FetchSpTAK(genus = "Abrothrix", species = "sanborni", synonyms = "Akodon sanborn
 syn$synonyms <- shQuote(syn$synonyms, "cmd")
 
 #list to get citation records
-scopus_out <- list() #initializing empty lists
-for (i in 1:length(sp$species)) {  
-  if (!sp$id[i] %in% syn$id) {
-    scopus_out[[i]] <- FetchSpTAK(genus = str_split(sp$species[i], pattern = " ")[[1]][1],
-                                  species = str_split(sp$species[i], pattern = " ")[[1]][2],
-                                  APIkey = "442b9048417ef20cf680a0ae26ee4d86")
+scopus_out1 <- list() #initializing empty lists
+for (i in 1:length(sp1$species)) {  
+  if (!sp1$id[i] %in% syn$id) {
+    scopus_out1[[i]] <- FetchSpTAK(genus = str_split(sp1$species[i], pattern = " ")[[1]][1],
+                                   species = str_split(sp1$species[i], pattern = " ")[[1]][2],
+                                   APIkey = "442b9048417ef20cf680a0ae26ee4d86")
   } else {
-   syns <- syn$synonyms[match(sp$id[i], syn$id)]
-   scopus_out[[i]] <- FetchSpTAK(genus = str_split(sp$species[i], pattern = " ")[[1]][1],
-                                 species = str_split(sp$species[i], pattern = " ")[[1]][2],
-                                 synonyms = syns,
-                                 APIkey = "442b9048417ef20cf680a0ae26ee4d86")
+   syns <- syn$synonyms[match(sp1$id[i], syn$id)]
+   scopus_out1[[i]] <- FetchSpTAK(genus = str_split(sp1$species[i], pattern = " ")[[1]][1],
+                                  species = str_split(sp1$species[i], pattern = " ")[[1]][2],
+                                  synonyms = syns,
+                                  APIkey = "442b9048417ef20cf680a0ae26ee4d86")
   }
 }
 
-saveRDS(scopus_out, "intermediate_data/scopus_results.RDS")
-
-
-#currently getting at error at index 14 and also later on
-indices <- list()
-for (i in 1:length(scopus_out)){
-  indices[[i]] <- Allindices(scopus_out[[i]],
-                             genus = str_split(sp$species[i], pattern = " ")[[1]][1],
-                             species = str_split(sp$species[i], pattern = " ")[[1]][2])
+scopus_out2 <- list() #initializing empty lists
+for (i in 1:length(sp2$species)) {  
+  if (!sp2$id[i] %in% syn$id) {
+    scopus_out2[[i]] <- FetchSpTAK(genus = str_split(sp2$species[i], pattern = " ")[[1]][1],
+                                   species = str_split(sp2$species[i], pattern = " ")[[1]][2],
+                                   APIkey = "442b9048417ef20cf680a0ae26ee4d86")
+  } else {
+    syns <- syn$synonyms[match(sp2$id[i], syn$id)]
+    scopus_out2[[i]] <- FetchSpTAK(genus = str_split(sp2$species[i], pattern = " ")[[1]][1],
+                                   species = str_split(sp2$species[i], pattern = " ")[[1]][2],
+                                   synonyms = syns,
+                                   APIkey = "442b9048417ef20cf680a0ae26ee4d86")
+  }
 }
 
-indices_df <- bind_rows(indices)
+saveRDS(scopus_out1, "intermediate_data/scopus_results1.RDS")
+saveRDS(scopus_out2, "intermediate_data/scopus_results2.RDS")
+
+#currently getting at error at index 14 and also later on
+indices1 <- list()
+for (i in 1:length(scopus_out1)){
+  indices1[[i]] <- Allindices(scopus_out1[[i]],
+                              genus = str_split(sp1$species[i], pattern = " ")[[1]][1],
+                              species = str_split(sp1$species[i], pattern = " ")[[1]][2])
+}
+
+indices2 <- list()
+for (i in 1:length(scopus_out2)){
+  indices2[[i]] <- Allindices(scopus_out2[[i]],
+                              genus = str_split(sp2$species[i], pattern = " ")[[1]][1],
+                              species = str_split(sp2$species[i], pattern = " ")[[1]][2])
+}
+
+indices1_df <- bind_rows(indices1)
+indices2_df <- bind_rows(indices2)
+indices_df <- rbind(indices1_df, indices2_df)
+
+write.csv(indices_df, file = "outputs/hindex")
 
 
 
