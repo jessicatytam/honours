6a1ff21c40a19cd2afd766e15af9d534dad3710a
jessicatytam <z5093095@student.unsw.edu.au>
2021-03-17 16:15:00 +1100

    phylosignal done! saved the lists too

--- R/phylogenetic_signal.R
+++ R/phylogenetic_signal.R
@@ -38,25 +38,76 @@ tree_test <- tree_node_dated$tree_7415
 
 table(tree_test$tip.label %in% includeh_tree$genus_species)
 
-body_mass<-as.array(includeh_tree$logmass)
-row.names(body_mass)<-includeh_tree$genus_species
-
-
 table(includeh_tree$genus_species %in% tree_test$tip.label)
-table(tree_test$tip.label %in% includeh_tree$genus_species)
+
+#prepare the data for the test
+
+h <- as.array(includeh_tree$logh1) #h-index
+row.names(h) <- includeh_tree$genus_species
+
+body_mass <- as.array(includeh_tree$logmass) #mass
+row.names(body_mass) <- includeh_tree$genus_species
+
+lat <- as.array(includeh_tree$median_lat) #latitude
+row.names(lat) <- includeh_tree$genus_species
+
+iucn <- as.array(includeh_tree$iucn_bin) #iucn
+row.names(iucn) <- includeh_tree$genus_species
+
+human_use <- as.array(includeh_tree$humanuse_bin) #human use
+row.names(human_use) <- includeh_tree$genus_species
+
+domestication <- as.array(includeh_tree$domestication_bin) #domestication
+row.names(domestication) <- includeh_tree$genus_species
+
+gtrends <- as.array(includeh_tree$gtrends_bin) #gtrends
+row.names(gtrends) <- includeh_tree$genus_species
+
+#check for na
 
 sum(is.na(body_mass))
+
 #phylo signal
+
+psignal_h <- phylosig(tree_test,
+                      x = h,
+                      test = TRUE) #k = 0.01398515
 
 psignal_mass <- phylosig(tree_test,
                          x = body_mass,
-                         test=TRUE) #this keeps crashing
+                         test = TRUE) #k = 0.2739998
 
-p4d <- phylo4d(tree_test, includeh_tree$h) #Number of labels does not match number of nodes
-phyloSignal(p4d = p4d, method = "all")
+psignal_lat <- phylosig(tree_test,
+                        x = lat,
+                        test = TRUE) #k = 0.03703145
+
+psignal_iucn <- phylosig(tree_test,
+                         x = iucn,
+                         test = TRUE) #k = 0.01600188
+
+psignal_use <- phylosig(tree_test,
+                        x = human_use,
+                        test = TRUE) #k = 0.02266098
+
+psignal_domestication <- phylosig(tree_test,
+                                  x = domestication,
+                                  test = TRUE) #k = 0.0109628
+
+psignal_gtrends <- phylosig(tree_test,
+                            x = gtrends,
+                            test = TRUE) #k = 0.02473557
 
 
+#save the data
 
+saveRDS(psignal_h, file = "outputs/stats/psignal_h.R")
+saveRDS(psignal_mass, file = "outputs/stats/psignal_mass.R")
+saveRDS(psignal_lat, file = "outputs/stats/psignal_lat.R")
+saveRDS(psignal_iucn, file = "outputs/stats/psignal_iucn.R")
+saveRDS(psignal_use, file = "outputs/stats/psignal_use.R")
+saveRDS(psignal_domestication, file = "outputs/stats/psignal_domestication.R")
+saveRDS(psignal_gtrends, file = "outputs/stats/psignal_gtrends.R")
 
+#combine and convert to df
 
-
+data.frame(psignal_h)
