fa4575f72a010cb5f1a7fb65ba273e95675d5eba
jessicatytam <z5093095@student.unsw.edu.au>
2021-05-05 10:31:20 +1000

    made a table of data summary

--- R/table_data_completeness.R
+++ R/table_data_completeness.R
@@ -2,22 +2,24 @@ library(formattable)
 
 includeh <- read.csv(file = "outputs/data/includeh.csv")[-c(1)] 
 
+#generate df
+
 table_df <- data.frame("Predictor" = c("Body mass",
                                        "Latitude",
-                                       "Phylogeny",
+                                       "Open Tree of Life phylogeny",
                                        "Phylogenetic tree",
                                        "IUCN Red List status",
                                        "Human use",
                                        "Domestication",
                                        "Google trends"),
-                       "Completeness" = c(paste0(round(5158/7522*100, digits = 1), "%"),
-                                          paste0(round(4435/7522*100, digits = 1), "%"),
-                                          paste0(round(6549/7522*100, digits = 1), "%"),
-                                          paste0(round(5498/7522*100, digits = 1), "%"),
-                                          paste0(round(5244/7522*100, digits = 1), "%"),
-                                          paste0(round(1397/7522*100, digits = 1), "%"),
-                                          paste0(round(7522/7522*100, digits = 1), "%"),
-                                          paste0(round(1323/7522*100, digits = 1), "%")),
+                       "Completeness" = percent(c(5158/7522,
+                                                  4435/7522,
+                                                  6549/7522,
+                                                  5498/7522,
+                                                  5244/7522,
+                                                  1397/7522,
+                                                  7522/7522,
+                                                  1323/7522)),
                        "Treatment" = c("Log transformation",
                                        "/",
                                        "/",
@@ -43,9 +45,47 @@ table_df <- data.frame("Predictor" = c("Body mass",
                                                 "No",
                                                 "No"))
 
+#renaming
+
+names(table_df)[names(table_df) == "Treatment.details"] <- "Treatment details"
+names(table_df)[names(table_df) == "Require.imputation"] <- "Require imputation"
+
+#formatter
+
+title_formatter <- formatter("span",
+                             style = x ~ style(color = "black",
+                                               "font-size" = 16,
+                                               "font-family" = "calibri",
+                                               "font-face" = "bold"))
+
+text_formatter <- formatter("span",
+                            style = x ~ style(color = "black",
+                                              "font-size" = 16,
+                                              "font-family" = "calibri"))
+
+bar_formatter <- formatter("span",
+                           style = x ~ style(
+                             color = "black",
+                             display = "inline-block",
+                             "background-color" = csscolor("lightblue"),
+                             width = percent(proportion(x)),
+                             "font-family" = "calibri",
+                             "font-size" = 16
+                           ))
+
+widget_formatter <- formatter("span",
+                              x ~ icontext(ifelse(x == "Yes", "ok", "remove"), ifelse(x == "Yes", "Yes", "No")),
+                              style = x ~ style("font-family" = "calibri",
+                                                "font-size" = 16,
+                                                color = ifelse(x == "Yes", "green", "red")))
+
+#table
+
+names(table_df) <- title_formatter(names(table_df)) #only run this once!
+
 formattable(table_df,
-            align = c("l"),
-            list("Completeness" = color_bar("#71CA97"),
-                 area(col = 2) ~ color_tile("#B1CBEB", "#3E7DCC"),
-                 "Require imputation" = formatter(icontext(c("Yes", "Yes", "No", "No", "Yes", "No", "No", "No")),
-                                                  style = x ~ style(color = "red", "green"))))
+            align = "l",
+            list(area(col = c(1, 3, 4)) ~ text_formatter,
+                 area(col = c(2)) ~ bar_formatter,
+                 area(col = c(5)) ~ widget_formatter))
+            
