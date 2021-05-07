3ca93c98b673992d1963ef96798c0836178d95da
jessicatytam <z5093095@student.unsw.edu.au>
2021-05-07 15:56:15 +1000

    R & pbs scipts

--- R/MCMCglmm_loop_3.R
+++ R/MCMCglmm_loop_3.R
@@ -8,17 +8,17 @@ imp_list <- readRDS("data/intermediate_data/MCMCglmm/imp_list.rds")
 random_trees <- readRDS("data/intermediate_data/random_trees.rds")
 
 #imputation list
-imp_1 <- imp_list[1:3]
+imp_3 <- imp_list[7:10]
 
 #non-informative prior
 prior1 <- list(R = list(V = diag(1), nu = 0.002),
                G = list(G1 = list(V = diag(1), nu = 1, alpha.mu = 0, alpha.V = diag(1)*1000)))
 
 #model
-mod_list_1 <- vector(mode = "list", length = 150)
+mod_list_3 <- vector(mode = "list", length = 200)
 for (i in 1:length(random_trees)) {
-  for (j in 1:length(imp_1)) {
-    mod_list_1[[j+(i-1)*length(imp_1)]] <- sprintf("%s is i and %s is j\n", i, j)
+  for (j in 1:length(imp_3)) {
+    mod_list_3[[j+(i-1)*length(imp_3)]] <- sprintf("%s is i and %s is j\n", i, j)
     # mod_list_1[[j+(i-1)*length(imp_1)]] <- MCMCglmm(h ~ logmass +
     #                                                 abs_lat +
     #                                                 humanuse_bin +
@@ -28,7 +28,7 @@ for (i in 1:length(random_trees)) {
     #                                               random = ~ animal,
     #                                               family = "poisson",
     #                                               pedigree = random_trees[[i]],
-    #                                               dat = imp_1[[j]],
+    #                                               dat = imp_3[[j]],
     #                                               nitt = 13000*10,
     #                                               thin = 10*10,
     #                                               burnin = 3000*10,
@@ -37,4 +37,4 @@ for (i in 1:length(random_trees)) {
 }
 
 #save the models
-saveRDS(mod_list_1, "data/intermediate_data/MCMCglmm/mod_list_1.rds")
+saveRDS(mod_list_3, "data/intermediate_data/MCMCglmm/mod_list_3.rds")
