0f12d9c68f70cd7d699c520c1b1d46fd851219e0
jessicatytam <z5093095@student.unsw.edu.au>
2021-05-08 08:52:43 +1000

    fixed R scripts

--- R/MCMCglmm_loop_3.R
+++ R/MCMCglmm_loop_3.R
@@ -18,21 +18,21 @@ prior1 <- list(R = list(V = diag(1), nu = 0.002),
 mod_list_3 <- vector(mode = "list", length = 100)
 for (i in 1:length(random_trees)) {
   for (j in 1:length(imp_3)) {
-    mod_list_3[[j+(i-1)*length(imp_3)]] <- sprintf("%s is i and %s is j\n", i, j)
-    # mod_list_1[[j+(i-1)*length(imp_1)]] <- MCMCglmm(h ~ logmass +
-    #                                                 abs_lat +
-    #                                                 humanuse_bin +
-    #                                                 domestication_bin +
-    #                                                 iucn_bin +
-    #                                                 log_sumgtrends,
-    #                                               random = ~ animal,
-    #                                               family = "poisson",
-    #                                               pedigree = random_trees[[i]],
-    #                                               dat = imp_3[[j]],
-    #                                               nitt = 13000*10,
-    #                                               thin = 10*10,
-    #                                               burnin = 3000*10,
-    #                                               prior = prior1)
+    #mod_list_3[[j+(i-1)*length(imp_3)]] <- sprintf("%s is i and %s is j\n", i, j)
+    mod_list_3[[j+(i-1)*length(imp_3)]] <- MCMCglmm(h ~ logmass +
+                                                    abs_lat +
+                                                    humanuse_bin +
+                                                    domestication_bin +
+                                                    iucn_bin +
+                                                    log_sumgtrends,
+                                                  random = ~ animal,
+                                                  family = "poisson",
+                                                  pedigree = random_trees[[i]],
+                                                  dat = imp_3[[j]],
+                                                  nitt = 13000*10,
+                                                  thin = 10*10,
+                                                  burnin = 3000*10,
+                                                  prior = prior1)
   }
 }
 
