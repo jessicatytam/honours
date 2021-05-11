04f059379ea31b9db9cbfa1cd434afddd00dc3c3
jessicatytam <z5093095@student.unsw.edu.au>
2021-05-11 16:13:48 +1000

    calculating mean and 95CI

--- R/MCMCglmm_results.R
+++ R/MCMCglmm_results.R
@@ -1,4 +1,6 @@
-library()
+library(purrr)
+library(tidyr)
+library(Rmisc)
 
 #load data
 includeh <- read.csv(file = "outputs/data/includeh.csv")[-c(1)] 
@@ -13,54 +15,32 @@ mod_list_5 <- readRDS("data/intermediate_data/MCMCglmm/mod_list_5.rds")
 #combine models
 mod_list_all <- do.call(c, list(mod_list_1, mod_list_2, mod_list_3, mod_list_4, mod_list_5))
 
-#look at the insides
-sum_mod <- summary(mod_list_all[[1]])
-sum_mod$Gcovariances[1] #animal_post.mean
-sum_mod$Rcovariances[1] #units_post.mean
-sum_mod$solutions[1,2] #mean, 95CI, p 
-
-#creating a list to store the data
+#create a list
 mod_results <- vector(mode = "list", length = 500)
-
-for (i in 1:length(mod_results)) {
-  sum_mod <- summary(mod_list_all[[i]])
-  mod_results[[i]] <- list(sum_mod$Gcovariances[1], sum_mod$Rcovariances[1], sum_mod$solutions)
-  print(paste("model", i, "extracted"))
+for (i in 1:length(mod_list_all)) {
+  mod_results[[i]] <- cbind(mod_list_all[[i]]$Sol[901:1000,], mod_list_all[[i]]$VCV[901:1000,])
 }
 
-#pagel's lambda
+mod_results_flat <- do.call(rbind, mod_results)
+mod_results_flat <- as.data.frame(mod_results_flat)
+
+#mean and 95CI
+
+mod_results_flat %>%
+  summarise_all(CI) #upper, mean, lower
+
+
+
+
+#pagel's lambda; animal_post.mean/(animal_post.mean + units_post.mean + v_dist) 
 dat_sub <- includeh %>%
   filter(genus_species %in% tree100$tree_6061$tip.label)
 
 v_dist <- log(1+ 1/mean(dat_sub$h)) #0.1054779
 
-for (i in 1:length(mod_results)) {
-  mod_results[[i]][[4]] <- mod_results[[i]][[1]] / (mod_results[[i]][[1]] + mod_results[[i]][[2]] + v_dist) 
-}
 
-#animal_post.mean/(animal_post.mean + units_post.mean + v_dist) 
 
 #getting the mean
-mod_mean <- vector(mode = "list", length = 15)
-
-for (i in 1:length(mod_results)) {
-  mod_mean[[1]] <- mod_results[[i]][[1]] #animal_post.mean
-  mod_mean[[2]] <- mod_results[[i]][[2]] #units_post.mean
-  mod_mean[[3]] <- mod_results[[i]][[4]] #pagel's lambda
-  mod_mean[[4]] <- mod_results[[i]][[3]][2,1] #mass_post.mean
-  mod_mean[[5]] <- mod_results[[i]][[3]][3,1] #lat_post.mean 
-  mod_mean[[6]] <- mod_results[[i]][[3]][4,1] #use_post.mean 
-  mod_mean[[7]] <- mod_results[[i]][[3]][5,1] #domestication_post.mean 
-  mod_mean[[8]] <- mod_results[[i]][[3]][6,1] #iucn_post.mean
-  mod_mean[[9]] <- mod_results[[i]][[3]][7,1] #gtrends_post.mean
-  mod_mean[[10]] <- mod_results[[i]][[3]][2,5] #mass_p
-  mod_mean[[11]] <- mod_results[[i]][[3]][3,5] #lat_p
-  mod_mean[[12]] <- mod_results[[i]][[3]][4,5] #use_p
-  mod_mean[[13]] <- mod_results[[i]][[3]][5,5] #domestication_p
-  mod_mean[[14]] <- mod_results[[i]][[3]][6,5] #iucn_p
-  mod_mean[[15]] <- mod_results[[i]][[3]][7,5] #gtrends_p
-  print(paste("model", i, "extracted"))
-}
 
 
 
